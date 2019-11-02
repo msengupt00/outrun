9979c21c0e0fa4aaa23e119f7e797c6d72f27b25
Mohona <mohonasengupta@Mohonas-MBP.fios-router.home>
2019-11-02 17:40:58 -0400

    this has the api bug in it

---
+++ OUTRUNscreens/OUTRUNscreens/Routes.swift
@@ -0,0 +1,34 @@
+//
+//  Routes.swift
+//  OUTRUNscreens
+//
+//  Created by Mohona Sengupta on 11/2/19.
+//  Copyright © 2019 Mohona Sengupta. All rights reserved.
+//
+
+import Foundation
+
+class Route {
+    var userTime: Int = 0
+    var userRadius: Float = 0
+    var apiKey = "AIzaSyD2sEnjE9LViaXMgvguCboMgDiaml1wdVY"
+    
+    func createRoute(_ userLocationLat: Int, _ userLocationLong: Int, _ userRadius: Int) {
+        
+//        let apicall: String = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + userLocationLat + "," + userLocationLong + "&radius=" + userRadius/2 + "&key= " + apiKey
+        
+        let apicall = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.445297,-79.941479&radius=1609.34&key=AIzaSyD2sEnjE9LViaXMgvguCboMgDiaml1wdVY"
+
+        
+        let locations = places(apicall)
+        
+        //let randInt = Int.random(in: 0 ..< locations.count)
+        
+        print("HE")
+        print(locations)
+//
+//        print("randInt: ", randInt)
+//        print("location: ", locations[randInt])
+        
+    }
+}
